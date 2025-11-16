7531ef09c1281792d4b26dc8c8a07ab6fe400ede
Xingyu Li <xl5936@nyu.edu>
2025-11-16 13:21:14 -0500

    set bucket name

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # AWS S3 Configuration
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME", "")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME", "polltest-resources")
 AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-west-2")
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 
